20dad18884c2cb610bd1a15773d4c46014bda6f5
EC2 Default User <ec2-user@ip-172-31-0-59.ec2.internal>
2021-06-20 03:05:21 +0000

    [Finish] 4-6

--- meshiterro/app/controllers/post_images_controller.rb
+++ meshiterro/app/controllers/post_images_controller.rb
@@ -1,5 +1,5 @@
 class PostImagesController < ApplicationController
-  
+
   def new
     @post_image = PostImage.new
   end
@@ -7,8 +7,13 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    # @post_image.save
+    # redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   def index
@@ -25,7 +30,7 @@ class PostImagesController < ApplicationController
     @post_image.destroy
     redirect_to post_images_path
   end
-  
+
   private
 
   def post_image_params
